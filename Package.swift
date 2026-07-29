8419279125595e9904d4578f38e3e59e270100e9
phil-c-arissian <philipcrossland@arissian.com>
2026-07-29 18:38:02 +0100

    Update ageProofVerifier to version 1.2.0-beta.4209

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AgeProofVerifier",
-            url: "https://github.com/luciditi-digital-id/age-proof-ios-verifier-packages/releases/download/v1.2.0-beta.4199/ageProofVerifier.xcframework.zip",
-            checksum: "8b77b169f669ad22e7e43e8391da27b708eeef9f6da283c1d466fbe85b85f9ba"
+            url: "https://github.com/luciditi-digital-id/age-proof-ios-verifier-packages/releases/download/v1.2.0-beta.4209/ageProofVerifier.xcframework.zip",
+            checksum: "18bc88b855208bb9aa44c5a75686a9fbedd4a7054436ceabbdf8944cc1093f56"
         )
     ]
 )
